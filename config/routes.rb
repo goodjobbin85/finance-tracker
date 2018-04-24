373e318f2258b7b1682d9d2a229b27cb23083845
Thomas Dubiel <timidturtle85@gmail.com>
2018-04-23 23:09:04 -0500

    stock lookup functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'stocks/search'
+
   get 'users/my_portfolio'
 
   devise_for :users
   root 'welcome#index'
   get 'my_portfolio', to: "users#my_portfolio"
+  get 'search_stocks', to: "stocks#search"
 
 end
