c7ce796e4d8b913d1626d849599e42b5f82fc484
Thomas Dubiel <timidturtle85@gmail.com>
2018-04-25 21:08:07 -0500

    refactor ajax code in stocks controller

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,19 +1,13 @@
 class StocksController < ApplicationController
   def search
-  	if params[:stock].present?
+  	if params[:stock].blank?
+  		flash.now[:danger] = "You've entered an empty search"
+  	else
 		@stock = Stock.new_from_lookup(params[:stock])
-	    if @stock
-	    	respond_to do |format|
-	 			format.js { render partial: 'users/result' }
-	 		end
-	 	else
-	 		flash[:danger] = "Please enter proper ticker symbol"
-			redirect_to my_portfolio_path
-		end
-	
-	else
-		flash[:danger] = "You've entered an empty search"
-		redirect_to my_portfolio_path
+		flash.now[:danger] = "Please enter proper ticker symbol" unless @stock 
+	end
+	respond_to do |format|
+		 format.js { render partial: "users/result" }
 	end
   end
 end
