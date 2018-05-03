0911fbe1ad7fc06e2073d0d5db1157e77be8feec
Thomas Dubiel <timidturtle85@gmail.com>
2018-05-03 09:55:58 -0500

    user search action and error handling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,14 @@ class UsersController < ApplicationController
   end
 
   def search 
-  	
+  	if params[:search_param].blank?
+  		flash.now[:danger] = "You've entered an empty search"
+  	else
+		@users = User.search(params[:search_param])
+		flash.now[:danger] = "No users found" if @users.blank?
+	end
+	respond_to do |format|
+		 format.js { render partial: "friends/result" }
+	end
   end
-
 end
